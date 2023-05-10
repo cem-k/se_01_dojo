87cdbba02cbd68e0b837819a67454009bcab1c62
Cem Kirciltepeli <cem.kirciltepeli@code.berlin>
2023-05-10 17:28:08 +0200

    loop until the game is finished

--- watersort.js
+++ watersort.js
@@ -4,6 +4,7 @@ const rl = require('readline').createInterface({
   });
 
 let from, to;
+let resume = true;
 
 const question1 = () => {
 return new Promise((resolve, reject) => {
@@ -18,7 +19,6 @@ const question2 = () => {
 return new Promise((resolve, reject) => {
     rl.question('To: ', (answer) => {
     to = answer;
-    console.log(pour())
     resolve();
     })
 })
@@ -28,12 +28,14 @@ return new Promise((resolve, reject) => {
 let beakers = {
     1: [1, 0, 0],
     2: [2, 1, 2],
-    3: [2, 1, 1]
+    3: [2, 2, 1]
 };
 
+let drop = [];
+
 // function to handle the pouring action
-function pour(){
-    let drop = [];
+function check(){
+    drop = [];
     let last = 0;
 
     // go in reverse through the from breaker to select the drops
@@ -44,10 +46,11 @@ function pour(){
                 last = beakers[from][i]
             } else if(beakers[from][i] == last){
                 drop.push(beakers[from][i]);
+            } else if(beakers[from][i] != last){
+                break;
             }
         }
     }
-    console.log(drop)
 
     // check if the pouring of the drop is possible
     for(let i = 0; i < beakers[to].length; i++){
@@ -61,9 +64,58 @@ function pour(){
     return false
 }
 
+function pour() {
+    console.log(drop)
+    // go in reverse through the from breaker to remove the drop
+    for(let i = beakers[from].length-1; i >= 0; i--){
+        if(beakers[from][i] != 0){
+            for(let j = 0; j < drop.length; j++){
+                beakers[from][i-j] = 0;
+            }
+            break;
+        }
+    }
+
+    // pour the drop to the other beaker
+    for(let i = 0; i < beakers[to].length; i++){
+        if(beakers[to][i] == 0){
+            for(let j = 0; j < drop.length; j++){
+                beakers[to][i+j] = drop[j];
+            }
+            break;
+        }
+    }
+}
+
+
+// check if the beakers are empty or contain only one liquid
+function checkFinish() {
+    for(const key in beakers){
+        let unique = new Set(beakers[key])
+        if(unique.size > 2 || (unique.size == 2 && !beakers[key].includes(0))){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+// main function to call the other function in order
 async function main(){
-    await question1()
-    await question2()
+    while(resume){
+        await question1()
+        await question2()
+        if(check()){
+            pour();
+            console.table(beakers)
+            resume = checkFinish()
+        } else {
+            console.log(drop)
+            console.log("Move not legal!")
+        }
+    }
+    console.log("You won!")
     rl.close()
 }
-main()
+
+main();
